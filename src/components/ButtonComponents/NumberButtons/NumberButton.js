194bb34145fccb570b2d6f84bdf45e07f6bbfa92
Modurotolu Olokode <durolawk@gmail.com>
2019-09-10 16:15:01 +0100

    Numbers component displayed

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+//import { numbers } from "../../../data";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  const {number} = props;
   return (
-    <>
+    <button>{number}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { specials } from "../components/data";
 
 //import any components needed
 
